651f5bf1c0a40597b02977bef36cbf9f667d8b5e
Alexandre Pawlicki Maule <108903118+AlexMaule@users.noreply.github.com>
2023-06-03 18:15:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import sqlalchemy
 import os
-# import numpy as np
+import numpy as np
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-# from flask_cors import CORS
+from flask_cors import CORS
 
 from flask import Flask, jsonify
 
@@ -20,7 +20,7 @@ accident = Base.classes.accidents
 
 # Flask Setup
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 # Flask Routes
 @app.route("/")
 def welcome():
